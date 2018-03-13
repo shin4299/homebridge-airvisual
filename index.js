e7f452e2983d83924abaa676ae00f27a05d28814
shin4299 <shin4299@gmail.com>
2018-03-13 22:52:29 +0900

    Update index.js

--- index.js
+++ index.js
@@ -193,12 +193,12 @@ AirVisualAccessory.prototype = {
                 that.conditions.aqi = parseFloat(that.standard === 'us' ? data.data.current.pollution.aqius : data.data.current.pollution.aqicn);
                 that.conditions.humidity = parseFloat(data.data.current.weather.hu);
                 that.conditions.temperature = parseFloat(data.data.current.weather.tp);
-                that.conditions.air_quality = that.convertAirQuality(that.conditions.aqi);
-                that.conditions.co = parseFloat(data.data.current.pollution.co);
+/*                that.conditions.co = parseFloat(data.data.current.pollution.co);
                 that.conditions.n2 = parseFloat(data.data.current.pollution.n2);
                 that.conditions.o3 = parseFloat(data.data.current.pollution.o3);
                 that.conditions.pm10 = parseFloat(data.data.current.pollution.p1);
-                that.conditions.s2 = parseFloat(data.data.current.pollution.s2);
+                that.conditions.s2 = parseFloat(data.data.current.pollution.s2);*/
+                that.conditions.air_quality = that.convertAirQuality(that.conditions.aqi);
 
                 that.log.debug('City is: %s', data.data.city);
                 that.log.debug('State is: %s', data.data.state);
